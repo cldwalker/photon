3dc00ed1dc79f42268cf6786e170430b4f81801d
Gabriel Horner <gabriel.horner@gmail.com>
2014-04-05 15:16:38 -0400

    remove unused generated code, add js.map

---
+++ photon_compiled.js
@@ -0,0 +1,12 @@
+if(!lt.util.load.provided_QMARK_('lt.plugins.photon')) {
+goog.provide('lt.plugins.photon');
+goog.require('cljs.core');
+goog.require('lt.objs.command');
+goog.require('lt.objs.command');
+goog.require('lt.object');
+goog.require('lt.object');
+lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword(null,"photon.add-folder","photon.add-folder",4332448574),new cljs.core.Keyword(null,"desc","desc",1016984067),"photon: TODO",new cljs.core.Keyword(null,"exec","exec",1017031683),(function (){return cljs.core.prn.call(null,"TODO");
+})], null));
+}
+
+//# sourceMappingURL=photon_compiled.js.map
